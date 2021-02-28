e2022bcb1aa5530d8b5ac87b0d53e981e9a97fa4
Andrew Shultz <shultz.ak@gmail.com>
2021-02-28 14:19:48 -0800

    Adding delimited string support for dictionary text
    Support for string delimited representations of dictionaries in either raw string or file format.
    
    Also includes test cases for new functionality.

---
+++ BoggleSolver/BoggleSolver.Lib/DictionaryReader/CharacterDelimitedStringReader.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BoggleSolver.Lib.DictionaryReader
+{
+    public class CharacterDelimitedStringReader : IDictionaryReader
+    {
+        private List<string> _words;
+        private readonly char _delimiter;
+        private int _currentPosition = 0;
+
+        public CharacterDelimitedStringReader(char delimiter, string rawContentsString = null)
+        {
+            _delimiter = delimiter;
+
+            SplitAndAddWordsToListFromString(rawContentsString);
+        }
+
+        public bool TryReadFromTextFile(string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                return false;
+            }
+
+            using(StreamReader reader = new StreamReader(filePath))
+            {
+                StringBuilder contents = new StringBuilder(string.Empty);
+
+                while(!reader.EndOfStream)
+                {
+                    //reading individual lines to prevent odd behaviors around line feeds being included in words
+                    contents.Append(reader.ReadLine());
+                }
+
+                SplitAndAddWordsToListFromString(contents.ToString());
+            }
+
+            return true;
+        }
+
+
+        public bool TryGetNextWord(out string nextWord)
+        {
+            if(_words == null || _words.Count <= 0 || _currentPosition >= _words.Count)
+            {
+                nextWord = null;
+                return false;
+            }
+
+            nextWord = _words[_currentPosition];
+            _currentPosition++;
+
+            return true;
+        }
+
+        private void SplitAndAddWordsToListFromString(string contents)
+        {
+            if(string.IsNullOrEmpty(contents) || _delimiter == '\0')
+            {
+                return;
+            }
+
+            string[] wordsFound = contents.Split(_delimiter);
+            _words = new List<string>(wordsFound.Length);
+
+            foreach (var word in wordsFound)
+            {
+                //We are not sanitizing here; functionally, this assumes however you have broken up your contents is "correct"
+                _words.Add(word);
+            }
+        }
+    }
+}
---
+++ BoggleSolver/BoggleSolver.UnitTests/DictionaryReaderTests.cs
@@ -0,0 +1,185 @@
+﻿using BoggleSolver.Lib.DictionaryReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BoggleSolver.UnitTests
+{
+    [TestClass]
+    public class DictionaryReaderTests
+    {
+        [TestMethod]
+        public void CharacterDelimitedReader_SingleWord()
+        {
+            string word = "word";
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(',', word);
+
+            Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+            Assert.AreEqual(word, nextWord);
+        }
+
+        [TestMethod]
+        public void CharacterDelimitedReader_RawString()
+        {
+            List<string> testWords = new List<string> { "word1", "word2", "word3", "word4","\t\t\t", " ", "" };
+            char delimiter = ',';
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(delimiter, string.Join(delimiter,testWords));
+
+            //order should match input
+            foreach(var word in testWords)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+        }
+
+        [TestMethod]
+        public void CharacterDelimitedReader_TryReadFromTextFile_SingleWord()
+        {
+            string textFileName = "testfile.txt";
+            string word = "word";
+
+            //Necessary as test may only run partially, leaving old file behind
+            if (File.Exists(textFileName))
+            {
+                File.Delete(textFileName);
+            }
+
+            using(StreamWriter writer = new StreamWriter(textFileName))
+            {
+                writer.Write(word);
+            }
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(',');
+
+            Assert.IsTrue(reader.TryReadFromTextFile(textFileName));
+            Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+            Assert.AreEqual(word, nextWord);
+
+            File.Delete(textFileName);
+        }
+
+        [TestMethod]
+        public void CharacterDelimitedReader_TryReadFromTextFile_SingleLine()
+        {
+            string textFileName = "testfile.txt";
+            List<string> testWords = new List<string> { "word1", "word2", "word3", "word4" };
+            char delimiter = ',';
+
+            //Necessary as test may only run partially, leaving old file behind
+            if (File.Exists(textFileName))
+            {
+                File.Delete(textFileName);
+            }
+
+            using (StreamWriter writer = new StreamWriter(textFileName))
+            {
+                writer.Write(string.Join(delimiter, testWords));
+            }
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(delimiter);
+            Assert.IsTrue(reader.TryReadFromTextFile(textFileName));
+
+            //order should match input
+            foreach (var word in testWords)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+        }
+
+        [TestMethod]
+        public void CharacterDelimitedReader_TryReadFromTextFile_MultipleLines_WindowsLineFeed()
+        {
+            string textFileName = "testfile.txt";
+            List<string> testWordsLine1 = new List<string> { "word1", "word2", "word3", "word4" };
+            List<string> testWordsLine2 = new List<string> { "word5", "word6", "word7", "word8" };
+            List<string> testWordsLine3 = new List<string> { "word9", "word10" };
+            char delimiter = ',';
+
+            //Necessary as test may only run partially, leaving old file behind
+            if (File.Exists(textFileName))
+            {
+                File.Delete(textFileName);
+            }
+
+            using (StreamWriter writer = new StreamWriter(textFileName))
+            {
+                writer.Write(string.Join(delimiter, testWordsLine1) + delimiter);
+                writer.Write("\r\n" + string.Join(delimiter, testWordsLine2) + delimiter);
+                writer.Write("\r\n" + string.Join(delimiter, testWordsLine3));
+            }
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(delimiter);
+            Assert.IsTrue(reader.TryReadFromTextFile(textFileName));
+
+            //order should match input
+            foreach (var word in testWordsLine1)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+
+            foreach (var word in testWordsLine2)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+
+            foreach (var word in testWordsLine3)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+        }
+
+        [TestMethod]
+        public void CharacterDelimitedReader_TryReadFromTextFile_MultipleLines_UnixLineFeed()
+        {
+            string textFileName = "testfile.txt";
+            List<string> testWordsLine1 = new List<string> { "word1", "word2", "word3", "word4" };
+            List<string> testWordsLine2 = new List<string> { "word5", "word6", "word7", "word8" };
+            List<string> testWordsLine3 = new List<string> { "word9", "word10" };
+            char delimiter = ',';
+
+            //Necessary as test may only run partially, leaving old file behind
+            if (File.Exists(textFileName))
+            {
+                File.Delete(textFileName);
+            }
+
+            using (StreamWriter writer = new StreamWriter(textFileName))
+            {
+                writer.Write(string.Join(delimiter, testWordsLine1) + delimiter);
+                writer.Write("\n" + string.Join(delimiter, testWordsLine2) + delimiter);
+                writer.Write("\n" + string.Join(delimiter, testWordsLine3));
+            }
+
+            CharacterDelimitedStringReader reader = new CharacterDelimitedStringReader(delimiter);
+            Assert.IsTrue(reader.TryReadFromTextFile(textFileName));
+
+            //order should match input
+            foreach (var word in testWordsLine1)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+
+            foreach (var word in testWordsLine2)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+
+            foreach (var word in testWordsLine3)
+            {
+                Assert.IsTrue(reader.TryGetNextWord(out string nextWord));
+                Assert.AreEqual(word, nextWord);
+            }
+        }
+    }
+}
